b9e87039bcde3eaed0b6b67042876ced969d2e81
Cedric <cedric.menanteau@epsi.fr>
2019-10-11 09:41:26 +0200

    Driver mannager + DB Connect

--- src/com/company/Data.java
+++ src/com/company/Data.java
@@ -1,4 +1,25 @@
 package com.company;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import SegaBank_B3C2.agence;
+
 public class Data {
+    private static final String DB_URL = "jdbc:mysql://localhost:3306/contact?useSSL=false&useLegacyDateTimeCode=false&serverTimeCode=utc";
+    private static final String DB_LOGIN = "root";
+    private static final String DB_PWD = "";
+
+    public static void main(String[] args){
+        try{
+            Connection c = DriverManager.getConnection("jdbc:mysql://localhost:3306/contact?useSSL=false&useLegacyDateTimeCode=false&serverTimeCode=utc", "root", "");
+            try{
+                PreparedStatement rq = c.prepareStatement("INSERT INTO agence (code, adresse) VALUES (?, ?)");
+                rq.setString(1, agence.getCode());
+                rq.setString(2, agence.getAdresse());
+            }
+        }
+    }
 }
